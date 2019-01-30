3a12b32f649be627e8758d262d8e035a5d9300cd
thuytrang-nguyen <43244612+thuytrang-nguyen@users.noreply.github.com>
2019-01-30 12:08:59 -0500

    Update dict.c

--- dict.c
+++ dict.c
@@ -3,6 +3,8 @@
 // DATE: December 17, 2018
 
 /*
+	BINARY TREE DICITONARY IMPLEMENTATION
+	
 	The dictionary consists of key-value pairs with key of string type and val of int type.
 	It holds unique keys.
 	Value -1 is reserved for returning value of failure to find a key in the dictionary.
@@ -11,6 +13,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
 
 // Struct for a pair holding a key and a value
 typedef struct Pair{
@@ -18,209 +23,235 @@ typedef struct Pair{
 	int value;
 }Pair;
 
-// Struct for a dictionary with a pointer to a pair as first and pointer to a dictionary as next
-typedef struct Dictionary{
-	Pair* first;
-	struct Dictionary* next;
-}Dictionary;
+// Struct for a tree dictionary with a pointer to a pair as item and pointers to a left and right node
+typedef struct BT_Dictionary{
+	Pair* item;
+	struct BT_Dictionary* left;
+	struct BT_Dictionary* right;
+}BT_Dictionary;
 
 // "Constructor" for a key-value pair - returns a pointer to a pair.
 Pair* newPair(char* k, int val) { 
     Pair* p =  (Pair*)malloc(sizeof(Pair)); 
-    p->key = k;
+    p->key = (char*)malloc(sizeof(k));
+    strncat(p->key, k, strlen(k));
     p->value = val; 
     return p; 
 } 
 
-// "Constructor" for dictionary - returns a pointer to a dictionary.
-Dictionary* dict(){
-	Dictionary* d = (Dictionary*)malloc(sizeof(Dictionary)); 
-	d->first = NULL;
-	d->next = NULL;
+// "Constructor" for tree dictionary - returns a pointer to a dictionary.
+BT_Dictionary* dict(){
+	BT_Dictionary* d = (BT_Dictionary*)malloc(sizeof(BT_Dictionary)); 
+	d->item = NULL;
+	d->left = NULL;
+	d->right = NULL;
 	return d;
 }
 
+
 // Method for checking if dictionary is empty. On true it returns 1 and on false 0.
-int dict_isEmpty(Dictionary* d){
-	if (d->first == NULL){
+int dict_isEmpty(BT_Dictionary* d){
+	if (d->item == NULL){
 		return 1;	
 	}else{
 		return 0;	
 	}
 }
 
-// Method returning the size of a dictionary.
-int dict_size(Dictionary* d){
-	int count = 0;
-	Dictionary* h = d;
-	while (h != NULL){
-		count++;
-		h = h->next;
+// Returns dictionary size
+int dict_size(BT_Dictionary* d){
+
+	if (d == NULL || d->item == NULL){
+		return 0;
+	}else{
+		return 1 + dict_size(d->left) + dict_size(d->right);
 	}
-	return count;
 }
 
-// Method returning the value of the given key if that key exists in the dictionary.
-// If the key is not found then -1 is returned.
-int dict_get(Dictionary* d, char* key){
- 	if(d->first == NULL){
- 		return -1;
- 	} 	
- 	Dictionary* h = d;
-	while (h != NULL){
-		if (strcmp((h->first)->key, key) == 0){
-			return (h->first)->value;
-		}
-		h = h->next;
+// If specified key is in dictionary, it returns the key's value, ow it returns -1
+int dict_get(BT_Dictionary* d, char* key){
+	if (d->item == NULL && d != NULL){
+		return -1;
 	}
-	return -1; 
-}
-
-// This is a helper function for the put method -> when a key already exists in the dictionary,
-// if a user tries to call put with a same key and some value, then the dictionary updates the pair's old value with the current one
-void updateVal(Dictionary* d, char* key, int val){
- 	Dictionary* h = d;
-	while (h != NULL){
-		if (strcmp((h->first)->key, key) == 0){
-			(h->first)->value = val;
+	char* k = (d->item)->key;
+	int v = (d->item)->value;
+	int cmp = strcmp(key, k);
+	if (cmp == 0){
+		return v;
+	}
+	else if (cmp > 0){
+		if (d->right != NULL){
+			return dict_get(d->right, key);
+		}else{
+			return -1;
 		}
-		h = h->next;
+	}
+	else{
+		if (d->left != NULL){
+			return dict_get(d->left, key);	
+		}else{
+			return -1;
+		}
 	}
 }
 
 // Method for adding new pairs into the dictionary
-void dict_put(Dictionary* d, char* key, int val){
-	if (val == -1){
-		printf("-1 value reserved, please use another one\n");
-		return;
+void dict_put(BT_Dictionary* d, char* key, int val){
+
+	// case when dictionary is empty
+	if (d->item == NULL && d !=NULL){
+		Pair* pair = newPair(key, val);	
+		d->item = pair;
 	}
-	if (key == NULL || val == NULL){
-		printf("You cannot use null keyes and values\n");
-		return;
-	}
-	int k = dict_get(d,key);
-	if (k<0){
-		Pair* pair = newPair(key, val);
-		if (d->first == NULL){
-			d->first = pair;
+	else{		
+		char* k = (d->item)->key;
+		int v = (d->item)->value;
+		int cmp = strcmp(key, k);
+
+		if (cmp==0){
+			(d->item)->value = val;
 		}
-		else if(d->next == NULL){
-			Dictionary* new = dict();
-			new->first = pair;
-			new->next = NULL;
-			d->next = new;
-
-		}else{
-			dict_put(d->next, key, val);
-		}
-	}else{
-		updateVal(d, key, val);
-	}
-}
-
-// This method removes a pair specified by the key from the dictionary
-void dict_remove(Dictionary* d, char* key){
-
-	int deleted = 0;
-	Dictionary* h = d;
-	Dictionary* prev = d;
-	while (h != NULL){
-		if (strcmp((h->first)->key, key) == 0){
-			Pair* rmv = h->first;   
-			prev->next = h->next;
-			free(rmv);
-			free(h);
-			deleted = 1;
-			h=NULL;
+		else if (cmp > 0){
+			if (d->right != NULL){
+				dict_put(d->right, key, val);
+			}else{
+				d->right = dict();
+				Pair* pair = newPair(key, val);	
+				(d->right)->item = pair;
+			}			
 		}
 		else{
-			prev = h;
-			h = h->next;
+			if (d->left != NULL){
+				dict_put(d->left, key, val);
+			}else{
+				d->left = dict();
+				Pair* pair = newPair(key, val);	
+				(d->left)->item = pair;
+			}	
+		}	
+	}
+}
+
+// print dictionary tree in order
+void print_node(BT_Dictionary* dict){
+	if  (dict != NULL){
+		Pair* x = dict->item;
+		printf("%s=%d, ", x->key, x->value);
+		print_node(dict->left);
+		print_node(dict->right);
+	}
+}
+void dict_print(BT_Dictionary* dict){
+	if (dict_isEmpty(dict)){
+		printf("{}\n");
+	}else{
+		printf("{");
+		print_node(dict);
+		printf("}\n");
+	}
+}
+
+// helper function for dict_free()
+void free_helper(BT_Dictionary* parent, int s, BT_Dictionary* d){
+
+	if (d->left == NULL && d->right == NULL){
+		free((d->item)->key);
+		free(d->item);
+		free(d);
+		if (s == 0){
+			parent->right = NULL;
+		}else{
+			parent->left = NULL;
+		}
+	}else{
+		if (d->left != NULL){
+			free_helper(d, 1, d->left);
+		}
+		if (d->right != NULL){
+			free_helper(d, 0, d->right);
 		}
 	}
-	if (deleted == 0){printf("Key not in dictionary\n");}
-
 }
+// Method for freeing all allocated memory
+void dict_free(BT_Dictionary* d){
 
-// This is a helper method for printing the contents of a dictionary.
-// It prints out one pair specified by x.
-void printPair(Pair* x){
-	char* p_str = (char*)malloc(sizeof(x)+2);
-	//printf("size size: %d\n", sizeof(x)+2);
-	memset(p_str, '\0', sizeof(x)+5);
-	strncat(p_str, x->key, strlen(x->key));
-	strncat(p_str, ":",1);
-	char b[(sizeof(int)*8+1)];
-	sprintf(b, "%d", x->value);
-	strcat(p_str,b);
-	printf("%s",p_str);
-	free(p_str); 
-}
-
-// Mehtod for printing out the contents of a dictionary. 
-// Format: {"key1":val1, "key2":val2 ...}
-void dict_print(Dictionary* d){
-	
-	Dictionary* h = d;
-	if(h->first==NULL){
-		printf("{}\n");
-		return;
+	while (dict_isEmpty(d) != 1){
+		// freeing the head (first item)
+		if (d->left == NULL && d->right == NULL){
+			Pair* to_free = d->item;
+			d->item = NULL;
+			free(to_free->key);
+			free(to_free);
+			free(d);
+		}
+		if (d->left != NULL){
+			free_helper(d, 1, d->left);
+		}
+		if (d->right != NULL){
+			free_helper(d, 0, d->right);
+		}
 	}
-	printf("{");
-	while (h != NULL){
-		printPair(h->first);
-		if (h->next != NULL){
-			printf(" ");
-		}		
-		h = (h->next);
 
-	}
-	printf("}");
-	printf("\n");
-}
-
-// Method for freeing all malloced memory from the dictionary.
-// At the end makes the dictionary empty.
-void dict_free(Dictionary* d){
-
-	Dictionary* h = d;
-	Dictionary* prev = NULL;
-	while (h->next != NULL){
-		prev = h;
-		h = h->next;
-	}
-	free(h->first);
-	free(h);
-	if(prev != NULL){
-		prev->next = NULL;
-		dict_free(d);
-	}else{
-		d->first = NULL;
-		d->next = NULL;
-	}
 }
 
 int main(int argc, char *argv[]) {
-	Dictionary* d = dict();
-	printf("Is it empty?: %d\n", dict_isEmpty(d));
-	dict_put(d, "super", 1);
-	dict_put(d, "tired", 2);
-	dict_put(d, "of", 3);
-	dict_put(d, "cs",4);
-	dict_put(d, "...",70000);
-	dict_print(d);
-	printf("Size of dict: %d\n", dict_size(d));
-	printf("get(...): %d\n", dict_get(d, "..."));
-	printf("get(fqf): %d\n", dict_get(d, "fqf"));
-	dict_put(d,"super",20);
-	dict_print(d);
-	dict_remove(d, "cs");
-	dict_print(d);
-	//dict_put(d, "u", NULL);
-	dict_free(d);
-	dict_print(d);
-}
 
+	BT_Dictionary* d = dict();
+	
+	FILE *fd = fopen("macbeth-processed.txt","r");
+	if(fd == NULL){
+		fclose(fd);
+		exit(EXIT_FAILURE);
+	}
+	char *line = NULL;
+	ssize_t read;
+	size_t len = 0;
+	long long total_elasped = 0;
+	
+	struct timeval start, end;
+
+	while ((read=getline(&line, &len, fd)) != -1){
+		char* token = strtok(line, " ");
+		while(token != NULL){
+			
+			gettimeofday(&start, NULL);
+			long long start_msec = start.tv_sec*1000LL + start.tv_usec/1000; // calculate milliseconds
+			int val = dict_get(d, token);
+			if (val == -1){
+				dict_put(d, token, 1);
+			}else{
+				dict_put(d, token, val+1);
+			}
+			gettimeofday(&end, NULL);
+			long long end_msec = end.tv_sec*1000LL + end.tv_usec/1000; // calculate milliseconds
+			
+			total_elasped += end_msec-start_msec;
+			token = strtok(NULL, " ");
+		}
+	}
+	fclose(fd);
+	free(line);
+	//dict_print(d);
+	//printf("\n");
+	printf("Time elasped of put in milliseconds: %lld\n", total_elasped);
+	printf("Average time of put per argument in milliseconds %lld\n", (total_elasped)/dict_size(d));
+	gettimeofday(&start, NULL);
+	printf("dict_get(macbeth): %d\n", dict_get(d, "macbeth"));
+	printf("dict_get(duncan): %d\n", dict_get(d, "duncan"));
+	printf("dict_get(malcolm): %d\n", dict_get(d, "malcolm"));
+	printf("dict_get(banquo): %d\n", dict_get(d, "banquo"));	
+	printf("dict_get(war): %d\n", dict_get(d, "war"));
+	printf("dict_get(treasonous): %d\n", dict_get(d, "treasonous"));
+	gettimeofday(&end, NULL);		
+	long long start_msec = start.tv_sec*1000LL + start.tv_usec/1000; // calculate milliseconds
+	long long end_msec = end.tv_sec*1000LL + end.tv_usec/1000; // calculate milliseconds
+	total_elasped = end_msec-start_msec;
+	printf("Time elasped of get in milliseconds: %lld\n", total_elasped);
+	printf("Average time of get per argument in milliseconds %lld\n", (total_elasped)/6);
+	printf("Size of c dictionary (BST): %d\n", dict_size(d));
+	dict_free(d);
+	return 0;
+}
 
 
 
